e64eb7fd6ff7f4158d71374559557fa7ef0770fc
Ines Ivanova <ines.iv.ivanova@gmail.com>
2020-03-01 14:02:25 +0200

    cors

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\netitstudents\1.03-exercise\be\finalrestba\final-django-rest-project\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
